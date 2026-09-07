e4f154555c4c15bd7aabe3b5bfe40578fb5bf26e
Lezane <45308492+Lezane@users.noreply.github.com>
2026-09-07 14:16:43 +0200

    Update factory.py

--- optimizers/factory.py
+++ optimizers/factory.py
@@ -1,20 +1,34 @@
 import torch.optim as optim
 from .muon import Muon
-from .config import HYPERPARAMS
+import config  # Import the new unified config
 
-def get_optimizers(opt_name, model, dataset):
-    hp = HYPERPARAMS[dataset][opt_name]
-    
+def get_optimizers(opt_name, model):
     if opt_name == 'AdamW':
-        return [optim.AdamW(model.parameters(), lr=hp['lr'], weight_decay=hp['weight_decay'])]
+        return [optim.AdamW(
+            model.parameters(), 
+            lr=config.ADAM_LEARNING_RATE,
+            betas=(config.ADAM_BETA1, config.ADAM_BETA2),
+            eps=config.ADAM_EPS,
+            weight_decay=config.ADAM_WEIGHT_DECAY
+        )]
     elif opt_name == 'SGD':
-        return [optim.SGD(model.parameters(), lr=hp['lr'], momentum=hp['momentum'], weight_decay=hp['weight_decay'])]
+        return [optim.SGD(
+            model.parameters(), 
+            lr=config.SGD_LEARNING_RATE, 
+            momentum=config.SGD_MOMENTUM
+        )]
     elif opt_name == 'Muon':
         muon_params = [p for p in model.parameters() if p.ndim >= 2]
         other_params = [p for p in model.parameters() if p.ndim < 2]
         return [
-            Muon(muon_params, lr=hp['lr'], momentum=hp['momentum']), 
-            optim.AdamW(other_params, lr=hp['adamw_lr'], weight_decay=hp['adamw_wd'])
+            Muon(muon_params, lr=config.MUON_LEARNING_RATE, momentum=config.MUON_MOMENTUM), 
+            optim.AdamW(
+                other_params, 
+                lr=config.ADAM_LEARNING_RATE,
+                betas=(config.ADAM_BETA1, config.ADAM_BETA2),
+                eps=config.ADAM_EPS,
+                weight_decay=config.ADAM_WEIGHT_DECAY
+            )
         ]
     else:
         raise ValueError(f"Unknown optimizer: {opt_name}")
